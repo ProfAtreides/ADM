f822a48ed54dd3a8dcdb0250a271f7e56527a30d
Dave <dambrozewicz7@gmail.com>
2025-12-21 03:52:48 +0100

    added regionprops useless for now

--- mask.py
+++ mask.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.measure import regionprops, label
 
 def transform_image_into_mask(image_path):
     image = cv2.imread(image_path)
@@ -26,9 +27,9 @@ Z = np.float32(Z)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 K = 3
-ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+ret, k_label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
-res = center[label.flatten()]
+res = center[k_label.flatten()]
 segmented_image = res.reshape((gauss.shape))
 
 # Yellow to blue idk why
@@ -37,21 +38,42 @@ segmented_gray = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2GRAY)
 min_val = np.min(segmented_gray)
 final_mask = np.where(segmented_gray == min_val, 0, 255).astype(np.uint8)
 
-fig, ax = plt.subplots(1, 4, figsize=(16,4))
-ax[0].imshow(image, cmap="gray")
-ax[0].set_title("Image")
-ax[0].axis("off")
+labeled_mask = label(final_mask)
+regions = regionprops(labeled_mask)
 
-ax[1].imshow(mask, cmap="gray")
-ax[1].set_title("Binary Mask")
-ax[1].axis("off")
+region_image = np.zeros_like(final_mask, dtype=np.uint8)
 
-ax[2].imshow(segmented_gray)
-ax[2].set_title("GaussBlur + KMeans")
-ax[2].axis('off')
+for region in regions:
+    print(region.area)
+    if region.area <= 25000000:  # Filtracja na podstawie obszaru
+        minr, minc, maxr, maxc = region.bbox
+        cv2.rectangle(region_image, (minc, minr), (maxc, maxr), 255, 2)
 
-ax[3].imshow(final_mask, cmap="gray")
-ax[3].set_title("Mask + filters")
-ax[3].axis('off')
+cleared_mask = cv2.bitwise_xor(final_mask, region_image)
+
+fig, ax = plt.subplots(2, 3, figsize=(12,8))
+ax[0,0].imshow(image, cmap="gray")
+ax[0,0].set_title("Image")
+ax[0,0].axis("off")
+
+ax[0,1].imshow(mask, cmap="gray")
+ax[0,1].set_title("Binary Mask")
+ax[0,1].axis("off")
+
+ax[0,2].imshow(segmented_gray)
+ax[0,2].set_title("GaussBlur + KMeans")
+ax[0,2].axis('off')
+
+ax[1,0].imshow(final_mask, cmap="gray")
+ax[1,0].set_title("Mask + filters")
+ax[1,0].axis('off')
+
+ax[1,1].imshow(region_image, cmap="gray")
+ax[1,1].set_title("Regionprops")
+ax[1,1].axis('off')
+
+ax[1,2].imshow(cleared_mask, cmap="gray")
+ax[1,2].set_title("XOR")
+ax[1,2].axis('off')
 
 plt.show()
